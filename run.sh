e4bc48d38e8496e84d461b0d631422f18d2f8534
ajuvi <102821845+ajuvi@users.noreply.github.com>
2026-02-02 07:59:55 +0100

    Update API endpoint in run.sh script

--- run.sh
+++ run.sh
@@ -5,23 +5,23 @@ read -p "Entra una operació (+,-,*,/): " operacio
 
 if [ "$operacio" = "+" ]; then
 	#envia a través del mètode HTTP GET
-	curl "http://localhost/php-rest-calc/rest.php?num1=80&num2=5"
+	curl "http://localhost/php-rest-calc/calculadora.php?num1=80&num2=5"
 
 elif [ "$operacio" = "-" ]; then
 	#envia a través del mètode HTTP POST
-	curl -X POST http://localhost/php-rest-calc/rest.php \
+	curl -X POST http://localhost/php-rest-calc/calculadora.php \
 	     -H "Content-Type: application/json" \
 	     -d '{"num1": "80", "num2": "5"}'
 
 elif [ "$operacio" = "*" ]; then
 	#envia a través del mètode HTTP PUT
-        curl -X PUT http://localhost/php-rest-calc/rest.php \
+        curl -X PUT http://localhost/php-rest-calc/calculadora.php \
              -H "Content-Type: application/json" \
              -d '{"num1": "80", "num2": "5"}'
 
 elif [ "$operacio" = "/" ]; then
 	#envia a través del mètode HTTP DELETE
-        curl -X DELETE http://localhost/php-rest-calc/rest.php \
+        curl -X DELETE http://localhost/php-rest-calc/calculadora.php \
              -H "Content-Type: application/json" \
              -d '{"num1": "80", "num2": "5"}'
 
