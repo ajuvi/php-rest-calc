0badcff203a373ce5195705b839ff85fadd73bc3
ajuvi <102821845+ajuvi@users.noreply.github.com>
2026-02-02 08:01:05 +0100

    Update API endpoint from rest.php to calculadora.php

--- run2.sh
+++ run2.sh
@@ -6,23 +6,23 @@ read -p "Entra una operació (+,-,*,/): " operacio
 
 if [ "$operacio" = "+" ]; then
         #envia a través del mètode HTTP GET
-        curl "http://localhost/rest-php-calc/rest.php?num1=${n1}&num2=${n2}"
+        curl "http://localhost/rest-php-calc/calculadora.php?num1=${n1}&num2=${n2}"
 
 elif [ "$operacio" = "-" ]; then
         #envia a través del mètode HTTP POST
-        curl -X POST http://localhost/rest-php-calc/rest.php \
+        curl -X POST http://localhost/rest-php-calc/calculadora.php \
              -H "Content-Type: application/json" \
              -d "{\"num1\": $n1, \"num2\": $n2}"
 
 elif [ "$operacio" = "*" ]; then
         #envia a través del mètode HTTP PUT
-        curl -X PUT http://localhost/rest-php-calc/rest.php \
+        curl -X PUT http://localhost/rest-php-calc/calculadora.php \
              -H "Content-Type: application/json" \
              -d "{\"num1\": $n1, \"num2\": $n2}"
 
 elif [ "$operacio" = "/" ]; then
         #envia a través del mètode HTTP DELETE
-        curl -X DELETE http://localhost/rest-php-calc/rest.php \
+        curl -X DELETE http://localhost/rest-php-calc/calculadora.php \
              -H "Content-Type: application/json" \
              -d "{\"num1\": $n1, \"num2\": $n2}"
 
